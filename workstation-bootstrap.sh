b8b04b6e6838d50b01a8068c6f85bbc89746f1d2
Andy Brown <andymoe@gmail.com>
2016-03-20 23:25:08 -0700

    Use functions for each install group
    - Namespace all functions suing prefix in case
      a tool needs to be run once it's installed
      (for example shellcheck.)
    
    - Strip function namespace prefix when priting
      name of just installed tool.

--- workstation-bootstrap.sh
+++ workstation-bootstrap.sh
@@ -16,25 +16,24 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#helpers
+
 wb_install () {
 	if (( $# != 2)); then
 		echo "Usage: wb_install install_func executable_name"
 		exit 1
 	fi
-
 	local installation_func=$1
 	local executable_name=$2
-	
+
 	if [[ $executable_name ]]; then
-		which -s "$executable_name"	
+		which -s "$executable_name"
 	fi
 
 	if [[ $? != 0 ]]; then
 		echo "installing ${installation_func}"
 		$installation_func
 	else
-		echo "${installation_func} already installed"
+		echo "${installation_func#*_} already installed"
 	fi
 }
 
@@ -57,90 +56,92 @@ wb_replace_system_version_via_brew () {
 	if [[ $? != 0 ]]; then
 		$installation_func
 	else
-		echo "${installation_func} already installed via Homebrew"
+		echo "${installation_func#*_} already installed via Homebrew"
 	fi
 }
 
-## Prerequisites
-homebrew () {
-	# Install Homebrew from the internets. Toootally secure.
-	HOMEBREW_INSTALL=https://raw.githubusercontent.com/\
-	Homebrew/install/master/install
-	ACTIVE_RUBY=$(which ruby)
-	$ACTIVE_RUBY -e \ "$(curl -fsSL "$HOMEBREW_INSTALL")"
-}
+wb_prerequisites () {
 
-bats () {
-	# Test framework for Bash scripts
-	brew install bats
-}
+	wb_homebrew () {
+		# Install Homebrew from the internets. Toootally secure.
+		HOMEBREW_INSTALL=https://raw.githubusercontent.com/\
+		Homebrew/install/master/install
+		ACTIVE_RUBY=$(which ruby)
+		$ACTIVE_RUBY -e \ "$(curl -fsSL "$HOMEBREW_INSTALL")"
+	}
 
-nuke_rvm () {
-	# Because RVM is basically a bash virus 
-	# and does not play nice with rbenv
-	which -s rvm
-	if [[ $? != 0 ]]; then
-		echo "rvm not found, yay"
-		return 1
-	fi
+	wb_bats () {
+		# Test framework for Bash scripts
+		brew install bats
+	}
 
-	sed -i '' "/.rvm\/scripts\/rvm/d" "${HOME}/.bash_profile"
-	yes 'yes' | rvm implode 	 
-}
+	wb_nuke_rvm () {
+		# Because RVM is basically a bash virus
+		# and does not play nice with rbenv
+		which -s rvm
+		if [[ $? != 0 ]]; then
+			echo "RVM not found, yay"
+			return 1
+		fi
 
-install_prerequisites () {
+		sed -i '' "/.rvm\/scripts\/rvm/d" "${HOME}/.bash_profile"
+		yes 'yes' | rvm implode
+	}
+
 	wb_install homebrew brew
 	wb_install bats bats
-	nuke_rvm
+	wb_nuke_rvm
 }
 
-## Editors
-sublime3 () {
-	brew install caskroom/cask/brew-cask
-	brew tap caskroom/versions
-	brew  cask install sublime-text3
-}
+wb_editors () {
 
-macvim  () {
-	brew cask install macvim
-}
+	wb_sublime3 () {
+		brew install caskroom/cask/brew-cask
+		brew tap caskroom/versions
+		brew  cask install sublime-text3
+	}
 
-# Ruby tools
-rbenv () {
-	brew install rbenv
-}
+	wb_macvim  () {
+		brew cask install macvim
+	}
 
-bs_ruby_tools () {
-	wb_install rbenv rbenv
-}
-
-install_editors () {
 	wb_install sublime3 subl
 	wb_install macvim mvim
 }
 
-## Linting tools
-shellcheck () {
-	brew install shellcheck
+wb_ruby_tools () {
+
+	wb_rbenv () {
+		brew install rbenv
+	}
+
+	wb_install rbenv rbenv
 }
 
-install_linting_tools () {
-	wb_install shellcheck shellcheck
+wb_linting_tools () {
+	wb_shellcheck () {
+		brew install shellcheck
+	}
+
+	wb_install wb_shellcheck shellcheck
 }
 
-## git and tools
-git () {
-	brew install git
-}
+wb_git_and_tools () {
+	wb_git () {
+		brew install git
+	}
 
-install_git_and_tools () {
-	wb_replace_system_version_via_brew git git
+	wb_replace_system_version_via_brew wb_git git
 }
 
 #-----------------------------
 # Install program groups
 #-----------------------------
-install_prerequisites
-install_editors
-install_linting_tools
-install_git_and_tools
+wb_prerequisites
+wb_ruby_tools
+wb_editors
+wb_linting_tools
+wb_git_and_tools
+
+# Lint thyself!
+shellcheck "$0"
